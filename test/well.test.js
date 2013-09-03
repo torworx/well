b42d3f1dcb629b2afc8745d8a94102567f297fca
taoyuan <taoyuan@oyo.im>
2013-09-03 12:40:56 +0800

    refactor

--- test/well.test.js
+++ test/well.test.js
@@ -1,11 +1,16 @@
-
 var well = require('../');
 var t = require('chai').assert;
 require('chai').Assertion.includeStack = true;
 
 var fakePromise, sentinel, other;
-function identity(val) { return val; }
-function constant(val) { return function() { return val; }; }
+function identity(val) {
+    return val;
+}
+function constant(val) {
+    return function () {
+        return val;
+    };
+}
 
 sentinel = {};
 other = {};
@@ -30,101 +35,110 @@ describe('well', function () {
     });
 
 
-    it('should return a promise for a promise', function() {
+    it('should return a promise for a promise', function () {
         var result = well(fakePromise);
         t.isFunction(result.then);
     });
 
-    it('should not return the input promise', function() {
+    it('should not return the input promise', function () {
         var result = well(fakePromise, identity);
         t.isFunction(result.then);
         t.notStrictEqual(result, fakePromise);
     });
 
-    it('should return a promise that forwards for a value', function() {
+    it('should return a promise that forwards for a value', function () {
         var result = well(1, constant(2));
 
         t.isFunction(result.then);
 
         return result.then(
-            function(val) {
+            function (val) {
                 t.equal(val, 2);
             },
             t.fail
         );
     });
 
-    it('should invoke fulfilled handler asynchronously for value', function() {
-        var val = other;
+    describe('fulfilled handler', function () {
+        it('should invoke fulfilled handler synchronously for value', function (done) {
+            var val = other;
 
-        try {
-            return well({}, function() {
-                t.strictEqual(val, sentinel);
-            });
-        } finally {
-            val = sentinel;
-        }
+            try {
+                return well({}, function () {
+                    t.strictEqual(val, other);
+                    done();
+                });
+            } finally {
+                val = sentinel;
+            }
+        });
+
+        it('should invoke fulfilled handler synchronously for fake promise', function (done) {
+            var val = other;
+
+            try {
+                return well(fakePromise, function () {
+                    t.strictEqual(val, other);
+                    done();
+                });
+            } finally {
+                val = sentinel;
+            }
+        });
+
+        it('should invoke fulfilled handler synchronously for resolved promise', function (done) {
+            var val = other;
+
+            try {
+                return well(well.resolve(), function () {
+                    t.strictEqual(val, other);
+                    done();
+                }, t.fail);
+            } finally {
+                val = sentinel;
+            }
+        });
+
+        it('should invoke rejected handler synchronously for rejected promise', function (done) {
+            var val = other;
+
+            try {
+                well(well.reject(),
+                    t.fail, function () {
+                        t.strictEqual(val, other);
+                        done();
+                    }
+                );
+            } finally {
+                val = sentinel;
+            }
+        });
     });
 
-    it('should invoke fulfilled handler asynchronously for fake promise', function() {
-        var val = other;
 
-        try {
-            return well(fakePromise, function() {
-                t.strictEqual(val, sentinel);
-            });
-        } finally {
-            val = sentinel;
-        }
-    });
-
-    it('should invoke fulfilled handler asynchronously for resolved promise', function() {
-        var val = other;
-
-        try {
-            return well(well.resolve(), function() {
-                t.strictEqual(val, sentinel);
-            });
-        } finally {
-            val = sentinel;
-        }
-    });
-
-    it('should invoke rejected handler asynchronously for rejected promise', function() {
-        var val = other;
-
-        try {
-            well(well.reject(),
-                t.fail, function() { t.strictEqual(val, sentinel); }
-            );
-        } finally {
-            val = sentinel;
-        }
-    });
-
-    it('should support deep nesting in promise chains', function() {
+    it('should support deep nesting in promise chains', function (done) {
         var d, result;
 
         d = well.defer();
         d.resolve(false);
 
-        result = well(well(d.promise.then(function(val) {
+        result = well(well(d.promise.then(function (val) {
             var d = well.defer();
             d.resolve(val);
             return well(d.promise.then(identity), identity).then(
-                function(val) {
+                function (val) {
                     return !val;
                 }
             );
         })));
 
         result.done(
-            function(val) {
+            function (val) {
                 t.ok(val);
+                done();
             },
             t.fail
         );
     });
-
 
 });

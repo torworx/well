23b26cd95b47994f29413fce3adc170749710bce
taoyuan <taoyuan@oyo.im>
2013-09-08 12:15:35 +0800

    refactor

--- well.js
+++ well.js
@@ -907,20 +907,16 @@
         // Prefer setImmediate, cascade to node, vertx and finally setTimeout
         if (typeof setImmediate === 'function') {
             nextTick = setImmediate.bind(global);
-        } else if (typeof MessageChannel !== 'undefined') {
+        } else if(typeof MessageChannel !== 'undefined') {
             var channel = new MessageChannel();
             channel.port1.onmessage = drainQueue;
-            nextTick = function () {
-                channel.port2.postMessage(0);
-            };
+            nextTick = function() { channel.port2.postMessage(0); };
         } else if (typeof process === 'object' && process.nextTick) {
             nextTick = process.nextTick;
         } else if (typeof vertx === 'object') {
             nextTick = vertx.runOnLoop;
         } else {
-            nextTick = function (t) {
-                setTimer(t, 0);
-            };
+            nextTick = function(t) { setTimeout(t, 0); };
         }
 
         /**
